bcb5dc76472b344c622bb3d18d60ba8d1939a1af
NoeMurr <nosba95@yahoo.com>
2018-06-12 09:13:45 -0700

    aggiunto donwload della bag in fase di building

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 ./clean.sh
+curl http://robotics.ethz.ch/~asl-datasets/ijrr_euroc_mav_dataset/vicon_room1/V1_01_easy/V1_01_easy.bag -O
 cd ORB_SLAM2
 export ROS_PACKAGE_PATH=$ROS_PACKAGE_PATH:$(pwd)/Examples/Ros
 ./build.sh
